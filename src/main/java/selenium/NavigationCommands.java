440b215aeb376f69b66c06e84f240a3d0f128be6
kais <cisacova@usm.md>
2022-04-22 08:27:00 +0300

    Commit log4j

--- src/main/java/selenium/NavigationCommands.java
+++ src/main/java/selenium/NavigationCommands.java
@@ -5,37 +5,40 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationCommands {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Create a new instance of the FireFox driver
-        System.setProperty("webdriver.gecko.driver", "G:\\geckodriver-v0.29.0-win64\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "G:\\geckodriver-v0.30.0-win64\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
 
         // Open ToolsQA web site
-        String appUrl = "http://www.DemoQA.com";
+        String appUrl = "https://demoqa.com/";
         driver.get(appUrl);
 
-        // Click on Registration link
+        // Click on Elements link
 //        .//*[@class="category-cards"]
 //        .//*[@class="category-cards"]//*[.="Elements"]
 //        .//*[@class="card mt-4 top-card"]//.[.="Elements"]
 //        .//*[contains(text(),"Elem")]
-        driver.findElement(By.xpath(".//*[@class=\"category-cards\"]//*[.=\"Elements\"]")).click();
-
+        String elementsCardXpath = ".//*[@class='category-cards']//div[@class='card mt-4 top-card'][1]";
+        System.out.println(elementsCardXpath);
+        driver.findElement(By.xpath(elementsCardXpath)).click();
+        Thread.sleep(30000);
         // Go back to Home Page
         driver.navigate().back();
-
+        Thread.sleep(30000);
         // Go forward to Registration page
         driver.navigate().forward();
-
+        Thread.sleep(30000);
         // Go back to Home page
         driver.navigate().to(appUrl);
-
+        Thread.sleep(30000);
         // Refresh browser
         driver.navigate().refresh();
 
         // Close browser
 
         System.out.println("Closing browser");
+        Thread.sleep(3000);
         driver.close();
     }
 }
